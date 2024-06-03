b852ca450696cd129c99c42b7a0e7530f774598f
adhithaprakasan <adhithaprakasan55@gmail.com>
2024-06-03 18:51:34 +0400

    first commit

---
+++ fizzbuzzalgor.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+
+    if(num%3==0)
+    {
+        cout<<"Fizz";
+    }
+    if(num%5==0)
+    {
+        cout<<"Buzz";
+    }
+    return 0;
+}
+
+
+
+
